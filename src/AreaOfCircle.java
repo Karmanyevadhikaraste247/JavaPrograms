4581d79ded1d409409ed196946a2dfdac7ad42d8
ashu <ashutoshsoldier@gmail.com>
2026-07-30 17:02:17 +0530

    Add AreaOfCircle program

---
+++ src/AreaOfCircle.java
@@ -0,0 +1,12 @@
+//8. Find area of a circle given radius.
+
+public class AreaOfCircle {
+    public static void main(String[]args){
+
+        double r = 5;
+        double pi = 3.14;
+        double areaOfCircle = pi * r * r;
+        System.out.println("Area of circle: " + areaOfCircle);
+
+    }
+}
