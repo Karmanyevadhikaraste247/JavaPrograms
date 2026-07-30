763fe1748bce64e881c222bc7c3213e44594dae6
ashu <ashutoshsoldier@gmail.com>
2026-07-30 16:50:15 +0530

    Update PerimeterTriangle program

--- src/PerimeterTriangle.java
+++ src/PerimeterTriangle.java
@@ -1,3 +1,16 @@
 //7. Find area and perimeter of a rectangle.
+import java.util.Scanner;
 public class PerimeterTriangle {
+    public static void main(String[]args){
+        Scanner sc =  new Scanner(System.in);
+        System.out.println("Enter your length: ");
+        double l = sc.nextDouble();
+        System.out.println("Enter your width: ");
+        double w = sc.nextDouble();
+        double area = l * w;
+        System.out.println("Area: " + area);
+        double perimeter = (2 * l) + (2 * w);
+        System.out.println("Perimeter: " + perimeter);
+    }
+
 }
