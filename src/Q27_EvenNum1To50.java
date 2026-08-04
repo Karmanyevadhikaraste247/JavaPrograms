c767e07ea97a1cac835cebee15a8f1f0e2537667
ashu <ashutoshsoldier@gmail.com>
2026-08-04 17:03:26 +0530

    Add Q27_EvenNum1To50 program

---
+++ src/Q27_EvenNum1To50.java
@@ -0,0 +1,10 @@
+//27. Print all even numbers from 1 to 50.
+public class Q27_EvenNum1To50 {
+    public static void main(String[]args){
+        for(int i = 1; i<=50; i++){
+            if(i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
