48e3ca38d55ec7133628ced71562e9865efafd6d
Karmanyevadhikaraste247 <Ashutoshbca2025@gmail.com>
2026-08-04 19:57:17 +0530

    Add Q31_Factorial program

---
+++ src/Q31_Factorial.java
@@ -0,0 +1,14 @@
+//31. Find the factorial of a number.
+import java.util.Scanner;
+public class Q31_Factorial {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your number: ");
+        int n = sc.nextInt();
+        int fact = 1;
+        for(int i = 1; i <= n; i++ ){
+            fact = fact * i;
+        }
+        System.out.println("Factorial is: " + fact);
+    }
+}
