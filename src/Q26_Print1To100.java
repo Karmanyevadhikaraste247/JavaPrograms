bee2ffc51fdbcbddf4706e55a6f8f9c060580d18
ashu <ashutoshsoldier@gmail.com>
2026-08-04 16:54:33 +0530

    Add Q26_Print1To100 program

---
+++ src/Q26_Print1To100.java
@@ -0,0 +1,11 @@
+//26. Print numbers from 1 to 100.
+import java.util.Scanner;
+public class Q26_Print1To100 {
+    public static void main(String[]args){
+        //Scanner sc = new Scanner(System.in);
+        int n;
+        for( n = 1; n <= 100; n++){
+            System.out.println(n);
+        }
+    }
+}
