a8102f466f724448579af2368a7b13dfbdbb345e
Karmanyevadhikaraste247 <Ashutoshbca2025@gmail.com>
2026-08-10 17:46:52 +0530

    Add Q48_InvertedRightTri program

---
+++ src/Pattern/Q48_InvertedRightTri.java
@@ -0,0 +1,21 @@
+//48. Print an inverted right triangle pattern.
+//* * * * *
+//* * * *
+//* * *
+//* *
+//*
+package Pattern;
+import java.util.Scanner;
+public class Q48_InvertedRightTri {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n =  sc.nextInt();
+        for (int i = 1; i <= n; i++){
+            for(int j = 1 ;j <= n - i + 1; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
