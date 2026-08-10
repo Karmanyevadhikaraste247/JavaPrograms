97678a4a87855933eda3b38573f6ed442616a56e
Karmanyevadhikaraste247 <Ashutoshbca2025@gmail.com>
2026-08-10 17:51:43 +0530

    Add Q49_NumberPyramid program

---
+++ src/Pattern/Q49_NumberPyramid.java
@@ -0,0 +1,15 @@
+//49. Print a number pyramid: 1 / 12 / 123 / ...
+package Pattern;
+import java.util.Scanner;
+public class Q49_NumberPyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++){
+            for (int j = 1; j <= i; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
