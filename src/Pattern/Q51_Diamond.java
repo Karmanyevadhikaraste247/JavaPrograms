95bc77e52f283b40c8352f0442a7b45baf355200
Karmanyevadhikaraste247 <Ashutoshbca2025@gmail.com>
2026-08-11 16:27:28 +0530

    Add Q51_Diamond program

---
+++ src/Pattern/Q51_Diamond.java
@@ -0,0 +1,37 @@
+//51. Print a diamond pattern of stars.
+//    *
+//   * *
+//  * * *
+// * * * *
+//* * * * *
+// * * * *
+//  * * *
+//   * *
+//    *
+package Pattern;
+import java.util.Scanner;
+public class Q51_Diamond {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++) {
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+            for(int i = n-1; i>=1; i--){
+                for(int s = 1; s <= n-i; s++){
+                    System.out.print(" ");
+                }
+                for (int j = 1; j<=i;j++){
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+        }
+}
+
