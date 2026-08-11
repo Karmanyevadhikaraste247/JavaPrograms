72476f0d0a383ff689738e9ce885e70eaca1c543
Karmanyevadhikaraste247 <Ashutoshbca2025@gmail.com>
2026-08-11 17:33:26 +0530

    Add Q51_PascalTriangleUpToN program

---
+++ src/Pattern/Q52_PascalTriangleUpToN.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;                                               /*
+Original formula:
+C(n,r) = n! / (r!(n-r)!)
+
+Previous → Next formula:
+next = current × (n-r)/(r+1)
+                                                                        */
+public class Q52_PascalTriangleUpToN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++){
+            for(int s = 1; s <= n - i; s++)
+                System.out.print(" ");
+            int num = 1;
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num + " ");
+                num = num * (i - j) / (j);
+            }
+            System.out.println();
+        }
+    }
+}
+/*
+import java.util.Scanner;
+
+public class Q52_PascalTriangleUpToN {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++){
+
+            for(int s = 1; s <= n - i; s++)
+                System.out.print(" ");
+
+            int num = 1;
+
+            for(int j = 1; j <= i; j++){
+                System.out.print(num + " ");
+                num = num * (i - j) / j;
+            }
+
+            System.out.println();
+        }
+    }
+}
+*/
